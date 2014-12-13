24d1ebfbee140b7f8ffe7034c260237f722fa2d7
Alexander Rath <irath96@gmail.com>
2014-12-13 04:21:33 +0100

    Initial Commit

---
+++ jswf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jswf
+//
+//  Created by Alexander Rath on 13.12.14.
+//  Copyright (c) 2014 Alexander Rath. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
